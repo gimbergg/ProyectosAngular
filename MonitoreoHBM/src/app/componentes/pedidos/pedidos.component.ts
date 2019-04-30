2ed754b4a0bfedf093d1a606cb51f5c45d7c311d
LAE020690 <lifeaelg@gmail.com>
2019-04-30 18:26:46 -0400

    oficina

--- MonitoreoHBM/src/app/componentes/pedidos/pedidos.component.ts
+++ MonitoreoHBM/src/app/componentes/pedidos/pedidos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Injectable } from '@angular/core';
+import { MatDialog, MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { PedidosService } from '../../servicios/pedidos.service';
 import { Observable } from 'rxjs';
@@ -10,24 +11,28 @@ import { Pedidos } from 'src/app/model/pedidos.model';
   styleUrls: ['./pedidos.component.css']
 })
 export class PedidosComponent implements OnInit {
-
-  lista: Pedidos[];
+  title = 'Lista de Pedidos';
+  lista: any;
+  listaPedido: any;
   constructor(
     private pedidosService:PedidosService
   ) { }
 
+
   ngOnInit() {
-    console.log(this.pedidosService.getPedidos());
-    this.pedidosService.getPedidos().subscribe(data => {
-      this.lista = data.map(e => {
-        return {
-          PedidoID: e.payload.doc.id,
-          ...e.payload.doc.data()
-        } as Pedidos;
-      })
-      console.log(this.lista);
-      
-    });
+    this.getListPedido();
+  }
+
+  getListPedido() {
+    this.pedidosService.getPedidos()      
+      .subscribe(data => {
+        this.listaPedido = []
+        data.forEach(element => {
+          let x = element.payload.toJSON()
+          this.listaPedido.push(x);          
+        })
+        console.log(this.listaPedido);
+      });
   }
 
 }
--- MonitoreoHBM/src/app/servicios/pedidos.service.ts
+++ MonitoreoHBM/src/app/servicios/pedidos.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireDatabase, AngularFireList, AngularFireObject } from '@angular/fire/database';
 import { Pedidos } from '../model/pedidos.model'
+
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class PedidosService {
+  pedidos: AngularFireList<any>;     
   constructor(
-    private firestore: AngularFirestore
-    ) { }
+    private db: AngularFireDatabase,
+    private firestore:AngularFirestore
+  ){}
 
   getPedidos() {
-    return this.firestore.collection(':80/Pedido').snapshotChanges();
+    //return this.firestore.collection(':80/Pedido').snapshotChanges();
+    return this.db.list(':80/Pedido').snapshotChanges();
   }
 }
